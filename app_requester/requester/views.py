c514e89bf242ccb0bd5f27eb7edf02f3882f770f
Justin Wolf <jwolf@keansburg.k12.nj.us>
2016-07-28 09:01:13 -0400

    Created search page and app view

--- app_requester/requester/views.py
+++ app_requester/requester/views.py
@@ -3,16 +3,19 @@ from django.http import HttpResponse
 import requests, json
 
 def index(request):
-    things = iTunesSearch(request)
-    return render(request, 'requester/index.html', {'things':things})
+    if request.method == "POST":
+        query = request.POST.get('search')
+        results = iTunesSearch(request, query)
+        return render(request, 'requester/index.html', {'results':results})
+    else:
+        return render(request, 'requester/index.html')
 
 def app(request, lookup):
     app = iTunesLookup(request, lookup)
     return render(request, 'requester/app.html', {'app':app})
 
-def iTunesSearch(request):
-    search = 'youtube'
-    r = requests.get('https://itunes.apple.com/search?country=us&media=software&entity=iPadSoftware&term=' + search)
+def iTunesSearch(request, query):
+    r = requests.get('https://itunes.apple.com/search?country=us&media=software&entity=iPadSoftware&term=' + query)
     return r.json()
 
 def iTunesLookup(request, lookup):
